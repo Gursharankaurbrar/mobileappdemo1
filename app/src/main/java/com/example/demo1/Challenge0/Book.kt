bd1d236f79626fd2cfcc012cf4b2ed2884388f36
RRC-Gursharan <143541652+Gursharankaurbrar@users.noreply.github.com>
2025-01-16 23:16:38 -0600

    Defined methods in Library class

--- app/src/main/java/com/example/demo1/Challenge0/Book.kt
+++ app/src/main/java/com/example/demo1/Challenge0/Book.kt
@@ -1,4 +1,10 @@
 package com.example.demo1.Challenge0
 
-class Book {
+class Book(
+    var title: String,
+    var author: String
+){
+    override fun toString(): String {
+        return "Book: $title , Author: $author"
+    }
 }
--- app/src/main/java/com/example/demo1/Challenge0/Library.kt
+++ app/src/main/java/com/example/demo1/Challenge0/Library.kt
@@ -1,4 +1,20 @@
 package com.example.demo1.Challenge0
 
-class Library {
+class Library(
+    val booksList: MutableList<Book> = mutableListOf()
+) {
+    fun addBook(book: Book){
+        booksList.add(book)
+        println("Book added to the list.")
+    }
+
+    fun removeBook(title: String){
+        booksList.removeIf { it.title == title }
+    }
+
+    fun displayAvailableBooks(){
+        for(book in booksList){
+            println(book)
+        }
+    }
 }
